056930a9ee01bf709c9d87d5450e5896b674ba65
Kosty63 <snoob163@mail.ru>
2018-12-04 18:48:40 +0400

    Create indicationsFormCheck.js

---
+++ static/js/indicationsFormCheck.js
@@ -0,0 +1,21 @@
+
+    var monthNames = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь",
+    "Октябрь", "Ноябрь", "Декабрь"];
+
+    var date = new Date();
+
+    var inputPeriud = document.getElementById('periud');
+
+    if (date.getDate() < 20){
+
+        inputPeriud.value = monthNames[date.getMonth()];
+
+    }else {
+        if (date.getMonth() > 11){
+            inputPeriud.value = monthNames[0]
+        }else {
+        inputPeriud.value = monthNames[date.getMonth() + 1]
+        }
+    }
+
+    inputPeriud.setAttribute('readonly', 'readonly');
