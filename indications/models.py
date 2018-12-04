58218871b0507640f3430bc953f6381d80c288a9
Kosty63 <snoob163@mail.ru>
2018-12-04 10:05:35 +0400

    Update models.py
    Add periud

--- indications/models.py
+++ indications/models.py
@@ -4,6 +4,7 @@ class IndicationsModel(models.Model):
     ls = models.BigIntegerField()
     fullName = models.CharField(max_length=256)
     address = models.CharField(max_length=1000)
+    periud = models.CharField(max_length=256)
     firstCount = models.IntegerField()
     twoCount = models.IntegerField(blank=True, null=True)
     freeCount = models.IntegerField(blank=True, null=True)
